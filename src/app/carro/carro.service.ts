68d14fffabceed454e66534425f6a2a1a8d3ba6d
Ismael-Vilacian <ismaelv161@gmail.com>
2021-06-04 21:33:52 -0300

    Adicionado configurações

--- src/app/carro/carro.service.ts
+++ src/app/carro/carro.service.ts
@@ -1,9 +1,71 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { CarroDto } from 'src/model/carro-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarroService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private snackbar: MatSnackBar
+
+  ) { }
+
+  carros: CarroDto[];
+  url = `${environment.config.URL_API + '/cliente'}`;
+
+  listarCarro(): Observable<CarroDto[]> {
+    return this.httpClient.get<CarroDto[]>(this.url).pipe(
+      map((carros) => this.carros)
+    );
+  }
+
+  salvarCarro(carro: CarroDto): Observable<CarroDto> {
+    return this.httpClient.post<CarroDto>(this.url + '/add', carro).pipe(
+      map(obj => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  editarCarro(carro: CarroDto): Observable<CarroDto> {
+    return this.httpClient.put<CarroDto>(this.url + '/edit', carro).pipe(
+      map(obj => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
+  }
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackbar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      panelClass: isError ? ['msg-error'] : ['msg-success']
+    });
+  }
+
+  buscarCarroPorId(id: number): Observable<CarroDto> {
+    return this.httpClient.get<CarroDto>(this.url + '/' + id).pipe(
+      map((carro) => carro),
+      catchError( (e) => this.errorHandler(e))
+    );
+  }
+
+  deletarCarro(idUser: number): Observable<CarroDto> {
+    debugger
+    return this.httpClient.delete<CarroDto>(this.url + '/delete/' + idUser).pipe(
+      map((carro) => carro),
+      catchError( (e) => this.errorHandler(e))
+    );
+  }
 }
