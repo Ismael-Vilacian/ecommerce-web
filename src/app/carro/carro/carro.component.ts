150c7abdbc984db71136239bfb4b538b6a0e9166
Ismael-Vilacian <ismaelv161@gmail.com>
2021-06-04 21:33:23 -0300

    Configuraçõe carro

--- src/app/carro/carro/carro.component.ts
+++ src/app/carro/carro/carro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CarroDto } from 'src/model/carro-dto';
+import { CarroService } from '../carro.service';
 
 @Component({
   selector: 'app-carro',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarroComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private carroService: CarroService,
+    private location: Location,
+    private router: Router
+  ) { }
+  
+  displayedColumns: string[] = ['id', 'marca', 'modelo', 'cor', 'anoVeiculo', 'valorDiaria'];
+  
+  carro: CarroDto = { 
+    id: 0,
+    marca: '',
+    modelo: '',
+    cor: '',
+    placa: '',
+    anoVeiculo: '',
+    valorDiaria: 0
+  };
+
+  carros: CarroDto[];
+  dataSource: any;
 
   ngOnInit(): void {
+    this.carroService.listarCarro().subscribe(dados => {
+      this.carros = dados;
+      this.dataSource = this.carros;
+    })
+  }
+
+  salvar(): void{
+    this.carroService.salvarCarro(this.carro).subscribe((dados) => {
+      this.carroService.showMessage('Carro salvo com sucesso!', false);
+      this.carros.push(dados);
+      this.dataSource = this.carros;
+      location.reload();
+    });
   }
 
 }
